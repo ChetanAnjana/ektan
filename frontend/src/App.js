22b6edd70ab710e98e10f26c2d0385e11451cced
ChetanAnjana <bob@example.com>
2022-06-12 15:06:16 -0400

    day1-project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Ektan</a>
+      </header>
+      <main>list Growings</main>
+    </div>
+  )
+}
+
+export default App
